917ef2a7d22dd02018b1d96b608a2371d5628dcf
Santella-Daniele <55455877+Santella-Daniele@users.noreply.github.com>
2020-04-06 20:12:11 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+
+
+public class Main {
+	public static void main(String[] args) {
+	
+	Lista lista = new Lista();
+	
+	    lista.stampa();
+
+        lista.inserisci(4);
+        lista.stampa();
+
+        lista.inserisci(10);
+        lista.stampa();
+
+        lista.inserisci(2);
+        lista.stampa();
+
+        lista.inserisci(3);
+        lista.stampa();
+
+        lista.inserisci(5);
+        lista.stampa();
+
+        lista.rimuovi(4);
+        lista.stampa();
+
+        lista.rimuovi(10);
+        lista.stampa();
+
+        lista.rimuovi(55);
+
+        lista.rimuovi(2);
+        lista.stampa();
+    }
+}
+	
+
